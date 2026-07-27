654b0ac7e3ef778fa4d51b3796f80f4198f3d797
Zhoums396 <zhoums396@gmail.com>
2026-07-27 15:05:53 +0800

    Remove GeoCopilot plugin source

--- GeoCopilot-Next/docker/jupyter_server_config.py
+++
@@ -1,6 +0,0 @@
-"""Container defaults for one non-root OpenGeoLab/JupyterHub user."""
-
-c.ServerApp.root_dir = "/home/jovyan/work"
-c.ServerApp.ip = "0.0.0.0"
-c.ServerApp.open_browser = False
-c.MCPExtensionApp.mcp_port = 3001
